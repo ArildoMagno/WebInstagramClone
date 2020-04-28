58dc7ee6963fb797d1d233129070d2fe275387a7
Arildo <0040117@academico.ifmg.edu.br>
2020-04-28 02:28:21 -0300

    subindoTodoProjeto

--- api/server.js
+++ api/server.js
@@ -1,13 +1,26 @@
 var express = require('express'),
 	bodyParser = require('body-parser'),
+	multiparty = require('connect-multiparty'),
 	mongodb = require('mongodb'),
-	objectId = require('mongodb').ObjectId;
+	objectId = require('mongodb').ObjectId
+	fs = require('fs');
 
 var app = express();
 
 //body-parser
 app.use(bodyParser.urlencoded({ extended:true}));
 app.use(bodyParser.json());
+app.use(multiparty());
+
+app.use(function(req, res, next){
+
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	res.setHeader("Access-Control-Allow-Headers", "content-type");
+	res.setHeader("Access-Control-Allow-Credentials", true);
+
+	next();
+});
 
 var port = 8080;
 
@@ -29,26 +42,50 @@ app.get('/', function(req, res){
 //POST (create)
 app.post('/api', function(req, res){
 
-	var dados = req.body;
+	var date = new Date();
+	time_stamp = date.getTime();
 
-	db.open( function(err, mongoclient){
-		mongoclient.collection('postagens', function(err, collection){
-			collection.insert(dados, function(err, records){
-				if(err){
-					res.json({'status' : 'erro'});
-				} else {
-					res.json({'status' : 'inclusao realizada com sucesso'});
-				}
-				mongoclient.close();
+	var url_imagem = time_stamp + '_' + req.files.arquivo.originalFilename;
+
+	var path_origem = req.files.arquivo.path;
+	var path_destino = './uploads/' + url_imagem;
+
+	
+
+	fs.rename(path_origem, path_destino, function(err){
+		if(err){
+			res.status(500).json({error: err});
+			return;
+		}
+
+		var dados = {
+			url_imagem: url_imagem,
+			titulo: req.body.titulo
+		}
+
+		db.open( function(err, mongoclient){
+			mongoclient.collection('postagens', function(err, collection){
+				collection.insert(dados, function(err, records){
+					if(err){
+						res.json({'status' : 'erro'});
+					} else {
+						res.json({'status' : 'inclusao realizada com sucesso'});
+					}
+					mongoclient.close();
+				});
 			});
 		});
+
 	});
+
+	
 
 });
 
 
 //GET (ready)
 app.get('/api', function(req, res){
+	
 	db.open( function(err, mongoclient){
 		mongoclient.collection('postagens', function(err, collection){
 			collection.find().toArray(function(err, results){
@@ -82,14 +119,36 @@ app.get('/api/:id', function(req, res){
 
 });
 
+app.get('/imagens/:imagem', function(req, res){
+
+	var img = req.params.imagem;
+
+	fs.readFile('./uploads/'+img, function(err, content){
+		if(err){
+			res.status(400).json(err);
+			return;
+		}
+
+		res.writeHead(200, { 'content-type' : 'image/jpg'});
+		res.end(content);
+	})
+});
+
 
 //PUT by ID (update)
 app.put('/api/:id', function(req, res){
+	
 	db.open( function(err, mongoclient){
 		mongoclient.collection('postagens', function(err, collection){
 			collection.update(
 				{ _id : objectId(req.params.id) },
-				{ $set : { titulo : req.body.titulo}},
+				{ $push : 	{
+								comentarios : {
+									id_comentario : new objectId(),
+									comentario : req.body.comentario
+								}
+							}
+				},
 				{},
 				function(err, records){
 					if(err){
@@ -103,20 +162,33 @@ app.put('/api/:id', function(req, res){
 			);
 		});
 	});
+
 });
 
 
 //DELETE by ID (remover)
 app.delete('/api/:id', function(req, res){
+
 	db.open( function(err, mongoclient){
 		mongoclient.collection('postagens', function(err, collection){
-			collection.remove({ _id : objectId(req.params.id)}, function(err, records){
-				if(err){
-					res.json(err);
-				} else {
-					res.json(records);
+			collection.update(
+				{ }, 
+				{ $pull : 	{
+								comentarios: { id_comentario : objectId(req.params.id)}
+							}
+				},
+				{multi: true},
+				function(err, records){
+					if(err){
+						res.json(err);
+					} else {
+						res.json(records);
+					}
+
+					mongoclient.close();
 				}
-			});
+			);
 		});
 	});
+
 });
